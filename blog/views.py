62ab8dd83b5f8014b951d65a45495080b8784e3f
frigas002 <guilhermeteotonio02@gmail.com>
2026-06-25 23:07:34 -0300

    m

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()
-    ).order_by('published_date')
+    ).order_by('publicado')
     return render(request, 'blog/post_list.html', {'posts': posts})
